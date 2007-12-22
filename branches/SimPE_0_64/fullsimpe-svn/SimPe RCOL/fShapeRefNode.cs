5b3ef96a9caa6a1f18a9f3f5176a7d331830958d
pljones <pljones@b0317113-201e-0410-9332-a427c079c53a>
2007-12-22 22:24:55 +0000

    Branch point for 0_64_5 release candidate.

---
+++ branches/SimPE_0_64/fullsimpe-svn/SimPe RCOL/fShapeRefNode.cs
@@ -0,0 +1,78 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Ambertation                                     *
+ *   quaxi@ambertation.de                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace SimPe.Plugin
+{
+	/// <summary>
+	/// This Form is no longer used...
+	/// </summary>
+	public class fShapeRefNode : System.Windows.Forms.Form
+	{
+		private System.ComponentModel.IContainer components;
+
+		public fShapeRefNode()
+		{
+			//
+			// Erforderlich für die Windows Form-Designerunterstützung
+			//
+			InitializeComponent();			
+		}
+
+		/// <summary>
+		/// Die verwendeten Ressourcen bereinigen.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{								
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung. 
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// fShapeRefNode
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(6, 14);
+			this.ClientSize = new System.Drawing.Size(848, 302);
+			this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.Name = "fShapeRefNode";
+			this.Text = "fShapeRefNode";
+
+		}
+		#endregion		
+														
+	}
+}
