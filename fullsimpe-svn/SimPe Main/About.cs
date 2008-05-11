8d125ffecbdae412521dca3c0317d1df2af9f955
pljones <pljones@b0317113-201e-0410-9332-a427c079c53a>
2008-05-11 16:52:02 +0000

    Work in progress changes to Help->About

--- fullsimpe-svn/SimPe Main/About.cs
+++ fullsimpe-svn/SimPe Main/About.cs
@@ -31,27 +31,24 @@ namespace SimPe
 	/// </summary>
 	public class About : SimPe.Windows.Forms.HelpForm
     {
-		private System.Windows.Forms.RichTextBox rtb;
-		private System.Windows.Forms.Button button1;
-        private Button button2;
         private WebBrowser wb;
+        private RichTextBox rtb;
 		/// <summary>
 		/// Erforderliche Designervariable.
 		/// </summary>
 		private System.ComponentModel.Container components = null;
 
-        public About() 
-            :this(false)
-		{
-        }
-		public About(bool html)
+        public About() : this(false) { }
+
+		public About(bool html) : base()
 		{
 			//
 			// Erforderlich für die Windows Form-Designerunterstützung
 			//
 			InitializeComponent();
-            button2.BackColor = SystemColors.Control;
-            this.FormBorderStyle = FormBorderStyle.None;
+
+            //button1.Left = this.Width - button1.Width - 64;
+            button1.BackColor = SystemColors.Control;
 			           
             wb.Navigating += new WebBrowserNavigatingEventHandler(wb_Navigating);
             wb.Navigated += new WebBrowserNavigatedEventHandler(wb_Navigated);
@@ -101,75 +98,62 @@ namespace SimPe
 		private void InitializeComponent()
 		{
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
-            this.rtb = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.wb = new System.Windows.Forms.WebBrowser();
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.pnContent.SuspendLayout();
             this.SuspendLayout();
             // 
-            // rtb
+            // pnContent
             // 
-            this.rtb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtb.BackColor = System.Drawing.Color.White;
-            this.rtb.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.rtb.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.rtb.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtb.Location = new System.Drawing.Point(33, 132);
-            this.rtb.Name = "rtb";
-            this.rtb.ReadOnly = true;
-            this.rtb.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.rtb.Size = new System.Drawing.Size(724, 295);
-            this.rtb.TabIndex = 2;
-            this.rtb.Text = "";
-            this.rtb.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.rtb_LinkClicked);
-            this.rtb.Enter += new System.EventHandler(this.rtb_Enter);
+            this.pnContent.Controls.Add(this.rtb);
+            this.pnContent.Controls.Add(this.wb);
+            this.pnContent.Size = new System.Drawing.Size(722, 367);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(342, 170);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            // 
-            // button2
-            // 
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(695, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(64, 23);
-            this.button2.TabIndex = 4;
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Size = new System.Drawing.Size(85, 28);
+            this.button1.Text = "";
+            this.button1.Click += new System.EventHandler(this.button2_Click);
             // 
             // wb
             // 
             this.wb.AllowNavigation = false;
             this.wb.AllowWebBrowserDrop = false;
+            this.wb.Dock = System.Windows.Forms.DockStyle.Fill;
             this.wb.IsWebBrowserContextMenuEnabled = false;
-            this.wb.Location = new System.Drawing.Point(33, 132);
-            this.wb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb.Location = new System.Drawing.Point(0, 0);
+            this.wb.Margin = new System.Windows.Forms.Padding(4);
             this.wb.Name = "wb";
-            this.wb.Size = new System.Drawing.Size(728, 295);
-            this.wb.TabIndex = 5;
+            this.wb.Size = new System.Drawing.Size(722, 367);
+            this.wb.TabIndex = 4;
             this.wb.WebBrowserShortcutsEnabled = false;
+            // 
+            // rtb
+            // 
+            this.rtb.BackColor = System.Drawing.Color.White;
+            this.rtb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.rtb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtb.Location = new System.Drawing.Point(0, 0);
+            this.rtb.Margin = new System.Windows.Forms.Padding(4);
+            this.rtb.Name = "rtb";
+            this.rtb.ReadOnly = true;
+            this.rtb.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.rtb.Size = new System.Drawing.Size(722, 367);
+            this.rtb.TabIndex = 5;
+            this.rtb.Text = "";
             // 
             // About
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.ClientSize = new System.Drawing.Size(773, 443);
-            this.Controls.Add(this.wb);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.rtb);
-            this.Controls.Add(this.button1);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.ClientSize = new System.Drawing.Size(781, 568);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "About";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "About";
+            this.pnContent.ResumeLayout(false);
             this.ResumeLayout(false);
 
 		}
@@ -482,7 +466,6 @@ namespace SimPe
 
 		private void rtb_Enter(object sender, System.EventArgs e)
 		{
-			button1.Focus();
 		}
 
         private void button2_Click(object sender, EventArgs e)
--- fullsimpe-svn/SimPe Splash/HelpForm.cs
+++ fullsimpe-svn/SimPe Splash/HelpForm.cs
@@ -31,55 +31,17 @@ using Ambertation.Windows.Forms;
 
 namespace SimPe.Windows.Forms
 {
-    public partial class HelpForm : TransparentForm //Ambertation.Windows.Forms.LayeredForm
+    public partial class HelpForm : Form //TransparentForm //Ambertation.Windows.Forms.LayeredForm
     {
-        
-        static Image top, bottom, center;
-        Rectangle headerrect;
-
         public HelpForm()
-            : base()//Color.Transparent, new Size(781, 475))
         {            
-            
-            
             InitializeComponent();
-
-            this.MinimumSize = new Size(781, 475);
-            this.MaximumSize = new Size(781, 2048);
         }
 
-        protected override void OnCreateBitmap(Graphics g, Bitmap b)
+        protected override void OnResize(EventArgs e)
         {
-            //base.OnCreateBitmap(g, b);
-
-            if (top == null)
-            {
-
-                top = Image.FromStream(typeof(HelpForm).Assembly.GetManifestResourceStream("SimPe.Windows.Forms.img.top.png"));
-                center = Image.FromStream(typeof(HelpForm).Assembly.GetManifestResourceStream("SimPe.Windows.Forms.img.center.png"));
-                bottom = Image.FromStream(typeof(HelpForm).Assembly.GetManifestResourceStream("SimPe.Windows.Forms.img.bottom.png"));
-                headerrect = new Rectangle(0, 0, top.Width, top.Height);
-            }
-
-            g.DrawImage(top, new Point(0, 0));
-            int y = top.Height;
-            int my = b.Height - bottom.Height;
-            while (y < my)
-            {
-                g.DrawImage(center, new Point(0, y));
-                y += center.Height;
-            }
-            g.DrawImage(bottom, new Point(0, my));
-
-            g.Dispose();
-        }
-
-        protected override Rectangle HeaderRect
-        {
-            get
-            {
-                return headerrect;
-            }
+            panel3.Top = this.Height - panel3.Height + 1;
+            base.OnResize(e);
         }
     }
 }
