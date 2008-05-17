a3f8dfe489ff5a4bcb6e4ecc2494696227b386c5
pljones <pljones@b0317113-201e-0410-9332-a427c079c53a>
2008-05-17 06:40:17 +0000

    Revert half-baked attempt to fix Help->About... screen

--- branches/dev_pljones/fullsimpe/SimPe Splash/HelpForm.Designer.cs
+++ branches/dev_pljones/fullsimpe/SimPe Splash/HelpForm.Designer.cs
@@ -28,79 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HelpForm));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.pnContent = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(781, 129);
-            this.panel1.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.BackColor = System.Drawing.SystemColors.Control;
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(668, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(66, 27);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // pnContent
-            // 
-            this.pnContent.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)));
-            this.pnContent.BackColor = System.Drawing.Color.Transparent;
-            this.pnContent.Location = new System.Drawing.Point(34, 136);
-            this.pnContent.Name = "pnContent";
-            this.pnContent.Size = new System.Drawing.Size(722, 68);
-            this.pnContent.TabIndex = 3;
-            // 
-            // panel3
-            // 
-            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
-            this.panel3.Location = new System.Drawing.Point(0, 211);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(781, 58);
-            this.panel3.TabIndex = 4;
-            // 
-            // HelpForm
-            // 
-            this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.CancelButton = this.button1;
-            this.ClientSize = new System.Drawing.Size(781, 269);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.pnContent);
-            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "HelpForm";
             this.Text = "HelpForm";
-            this.panel1.ResumeLayout(false);
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        protected System.Windows.Forms.Panel pnContent;
-        protected System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel3;
     }
 }
