681ba5831ea595410417b4de46b9af74180a9f79
pljones <pljones@b0317113-201e-0410-9332-a427c079c53a>
2008-09-22 22:07:05 +0000

    0.72 (alpha) released

---
+++ tags/SimPe_0_72_00/fullsimpe/SimPe RCOL/cReferentNode.cs
@@ -0,0 +1,115 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Ambertation                                     *
+ *   quaxi@ambertation.de                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+using System;
+using System.Drawing;
+using System.Collections;
+using SimPe.Interfaces.Plugin;
+
+namespace SimPe.Plugin
+{
+	
+	/// <summary>
+	/// This is the actual FileWrapper
+	/// </summary>
+	/// <remarks>
+	/// The wrapper is used to (un)serialize the Data of a file into it's Attributes. So Basically it reads 
+	/// a BinaryStream and translates the data into some userdefine Attributes.
+	/// </remarks>
+	public class ReferentNode
+		: AbstractRcolBlock
+	{
+		#region Attributes
+	
+		#endregion
+		/*public Rcol Parent 
+		{
+			get { return parent; }
+		}*/
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public ReferentNode(Rcol parent) : base(parent)
+		{
+		}
+		
+		#region IRcolBlock Member
+
+		/// <summary>
+		/// Unserializes a BinaryStream into the Attributes of this Instance
+		/// </summary>
+		/// <param name="reader">The Stream that contains the FileData</param>
+		public override void Unserialize(System.IO.BinaryReader reader)
+		{
+			version = reader.ReadUInt32();
+		}
+
+		/// <summary>
+		/// Serializes a the Attributes stored in this Instance to the BinaryStream
+		/// </summary>
+		/// <param name="writer">The Stream the Data should be stored to</param>
+		/// <remarks>
+		/// Be sure that the Position of the stream is Proper on 
+		/// return (i.e. must point to the first Byte after your actual File)
+		/// </remarks>
+		public override void Serialize(System.IO.BinaryWriter writer)
+		{
+			writer.Write(version);
+		}
+
+		//fShapeRefNode form = null;
+		TabPage.GenericRcol tGenericRcol;
+		public override System.Windows.Forms.TabPage TabPage
+		{
+			get
+			{
+				if (tGenericRcol==null) tGenericRcol = new SimPe.Plugin.TabPage.GenericRcol();
+				return tGenericRcol;
+			}
+		}
+		#endregion
+
+		/// <summary>
+		/// You can use this to setop the Controls on a TabPage befor it is dispplayed
+		/// </summary>
+		protected override void InitTabPage() 
+		{
+			if (tGenericRcol==null) tGenericRcol = new SimPe.Plugin.TabPage.GenericRcol();
+			tGenericRcol.tb_ver.Text = "0x"+Helper.HexString(this.version);
+			tGenericRcol.gen_pg.SelectedObject = this;
+		}
+
+		public override string ToString()
+		{
+			return this.BlockName;
+		}
+
+
+		#region IDisposable Member
+
+		public override void Dispose()
+		{
+			if (this.tGenericRcol!=null) this.tGenericRcol.Dispose();
+			tGenericRcol = null;
+		}
+
+		#endregion
+	}
+}
