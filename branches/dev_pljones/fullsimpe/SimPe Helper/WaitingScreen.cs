50fc038438f32c221d8de019ee2c7bd79c3d8f0c
pljones <pljones@b0317113-201e-0410-9332-a427c079c53a>
2008-10-05 11:38:16 +0000

    Rewrite WaitingForm, modelled on SplashScreen/SplashForm

--- branches/dev_pljones/fullsimpe/SimPe Helper/WaitingScreen.cs
+++ branches/dev_pljones/fullsimpe/SimPe Helper/WaitingScreen.cs
@@ -1,6 +1,8 @@
 /***************************************************************************
  *   Copyright (C) 2005 by Ambertation                                     *
  *   quaxi@ambertation.de                                                  *
+ *   Copyright (C) 2008 by Peter L Jones                                   *
+ *   peter@users.sf.net                                                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -24,270 +26,116 @@ using System.Windows.Forms;
 
 namespace SimPe
 {
-	/// <summary>
-	/// Determins the Kind of Update the USer has Requested
-	/// </summary>
-	enum WSUpdate : byte 
-	{
-		Nothing = 0x0,
-		Image = 0x01,
-		Message = 0x02,
-		Both = 0x03
-	}
-
-	/// <summary>
-	/// Determins the current State of the WaitingScreen
-	/// </summary>
-	public enum WSState : byte
-	{
-		Inactive = 0x00,
-		Initializing = 0x01,
-		Running = 0x02,
-		Updating = 0x03,
-		WaitFinalizing = 0x04,
-		Finalized = 0x05
-	}
-
-	/// <summary>
-	/// Describes one Event that the user had requested
-	/// </summary>
-	class WSEvent 
-	{
-		internal WSEvent()
-		{
-			kind = WSUpdate.Nothing;
-		}
-
-		WSUpdate kind;
-		internal WSUpdate Kind 
-		{
-			get { return kind; }
-			set {kind = value; }
-		}
-
-		string msg;
-		internal string Message
-		{
-			get { return msg; }
-			set {msg = value; }
-		}
-
-		Image img;
-		internal Image Image
-		{
-			get { return img; }
-			set {img = value; }
-		}
-	}
-	/// <summary>
-	/// Use this class to show a Waiting Screen.
-	/// </summary>
-	public class WaitingScreen
-	{
-		static System.Threading.Thread thread = null;
-        static System.Threading.ManualResetEvent run = new ManualResetEvent(false);
-		static WSState state = WSState.Inactive;
-		static WSEvent nextevent = new WSEvent();
-		static TimeSpan ts = new TimeSpan(0, 0, 0, 0, 100);
-
-        static System.Threading.ManualResetEvent wait = new ManualResetEvent(true);
-        static System.Threading.ManualResetEvent ended = new ManualResetEvent(true);        
-        static  System.Threading.ManualResetEvent stop = new ManualResetEvent(false);        
-
-		/// <summary>
-		/// True if the WaitingScreen is available at the Moment
+    public class WaitingScreen
+    {
+        /// <summary>
+		/// Display a new WaitingScreen image
 		/// </summary>
-		public static bool Running
-		{
-			get {return ((state == WSState.Running) || (state == WSState.Updating) || (state == WSState.Initializing));}
-		}
-
+		/// <param name="image">the Image to show</param>
+        public static void UpdateImage(System.Drawing.Image image) { Screen.doUpdate(image); }
+        /// <summary>
+        /// Display a new WaitingScreen image and message
+        /// </summary>
+        /// <param name="both">the MessageAndImage to show</param>
+        public static void Update(System.Drawing.Image image, string msg) { Screen.doUpdate(image, msg); }
+        /// <summary>
+        /// Display a new WaitingScreen message
+        /// </summary>
+        /// <param name="msg">The Message to show</param>
+        public static void UpdateMessage(string msg) { Screen.doUpdate(msg); }
 		/// <summary>
-		/// Returns the current State of the WaitingScreen
+        /// Show the WaitingScreen
 		/// </summary>
-		public static WSState State 
-		{
-			get { return state; }
-		}
-
+        public static void Wait() { Screen.doWait(); }
+        /// <summary>
+        /// Stop the WaitingScreen and focus the given Form
+        /// </summary>
+        /// <param name="form">The form to focus</param>
+        public static void Stop(Form form) { Screen.doStop(); form.Activate(); }
 		/// <summary>
-		/// Show the Waitingscreen
+        /// Stop the WaitingScreen
 		/// </summary>
-        public static void Wait()
+        public static void Stop() { Screen.doStop(); }
+		/// <summary>
+		/// True if the WaitingScreen is displayed
+		/// </summary>
+        public static bool Running { get { return scr != null; } }
+        /// <summary>
+        /// Returns the Size of the Dispalyed Image
+        /// </summary>
+        public static System.Drawing.Size ImageSize { get { return new System.Drawing.Size(64, 64); } }
+
+
+        /// <summary>
+        /// Event1: StartThread has created frm and sent SetMessage
+        /// </summary>
+        static System.Threading.ManualResetEvent ev1 = new System.Threading.ManualResetEvent(false);
+        static WaitingScreen scr;
+        static object lockObj = new object();
+        static WaitingScreen Screen
         {
-            lock (wait)
-            {                
-                if (!Helper.WindowsRegistry.WaitingScreen) return;
-                run.Set();
-                //Console.WriteLine("starting...");
-
-                if (!Running)
+            get
+            {
+                if (scr == null)
                 {
-                    state = WSState.Initializing;
-
-                    thread = new Thread(new ThreadStart(Start));
-                    thread.IsBackground = true;
-                    thread.SetApartmentState(ApartmentState.STA);
-                    thread.Start();
-                    //Console.WriteLine("Started thread");
+                    ev1.Reset();
+                    scr = new WaitingScreen();
+                    ev1.WaitOne();
                 }
+                return scr;
             }
         }
-        
 
-		/// <summary>
-		/// Stop the WaitingScreen ad focus the given Form
-		/// </summary>
-		/// <param name="form">The form you want to focus</param>
-		public static void Stop(Form form) 
-		{
-			form.Activate();	
-			Stop();
-		}
 
-		/// <summary>
-		/// Stop the Waiting Thread
-		/// </summary>
-        public static void Stop()
+
+        System.Drawing.Image prevImage = null;
+        string prevMessage = "";
+        SimPe.WaitingForm frm;
+        System.Threading.Thread t = null;
+
+        void doUpdate(System.Drawing.Image image) { lock (lockObj) { prevImage = image; } if (frm != null) frm.SetImage(image); }
+        void doUpdate(string msg) { lock (lockObj) { prevMessage = msg; } if (frm != null) frm.SetMessage(msg); }
+        void doUpdate(System.Drawing.Image image, string msg) { doUpdate(image); doUpdate(msg); }
+        void doWait() { if (frm != null) frm.StartSplash(); }
+        void doStop() { if (frm != null) frm.StopSplash(); }
+
+
+
+        private WaitingScreen()
         {
-            if (!run.WaitOne(250, false)) return;
-            lock (wait)
+            if (Helper.WindowsRegistry.WaitingScreen)
             {
-                //Console.WriteLine("stopping...");
-
-                stop.Set();
-                ended.WaitOne(5000, false);
-                ended.Set();
-                Application.DoEvents();
-
-
-
-                thread = null;
-                state = WSState.Inactive;
-                //Console.WriteLine("Stopped thread");
+                t = new System.Threading.Thread(new System.Threading.ThreadStart(StartThread));
+                t.Start();
             }
-        }
-		
-		/// <summary>
-		/// Internal Method to start the Thread
-		/// </summary>
-        internal static void Start()
-        {            
-            if (stop.WaitOne(1, false))
-            {
-                ended.Set();
-                return;
-            }
-
-            stop.Reset();
-            WaitingForm wf = new WaitingForm();
-            lock (wait)
-            {
-                //Console.WriteLine("In Thread");
-                ended.Reset();
-
-                //Console.WriteLine("Init...");
-                state = WSState.Running;                
-                
-                wf.timer1.Enabled = true;
-                lock (nextevent)
-                {
-                    nextevent.Message = "";
-                    nextevent.Image = wf.pbsimpe.Image;
-                    nextevent.Kind = WSUpdate.Both;
-                }
-
-                wf.Visible = true;
-                wf.Update();
-                System.Windows.Forms.Application.DoEvents();
-            }
-            ended.Set();
-            StartUpdates(wf);
-
-            state = WSState.Finalized;
-            if (wf != null) wf.Visible = false;
-            wf = null;
-
-            ended.Set();
+            else
+                ev1.Set();
         }
 
-		/// <summary>
-		/// Updates to indicate activity are triggered by an additional Thread started where
-		/// </summary>
-        internal static void StartUpdates(WaitingForm wf)
+        protected void StartThread()
         {
-            //Console.WriteLine("Launched");
-            while (true)
+            frm = new SimPe.WaitingForm();
+            lock (lockObj)
             {
-                ended.Reset();
-                
-                if (state != WSState.Running) return;                
-
-                state = WSState.Updating;
-
-
-                lock (nextevent)
-                {
-                    if (((byte)nextevent.Kind & (byte)WSUpdate.Image) != 0) wf.ChangeImage(nextevent.Image);
-                    if (((byte)nextevent.Kind & (byte)WSUpdate.Message) != 0) wf.ChangeMessage(nextevent.Message);
-                    nextevent.Kind = WSUpdate.Nothing;
-                }
-
-                wf.Update();
-                System.Windows.Forms.Application.DoEvents();
-
-
-                if (state == WSState.Updating) state = WSState.Running;
-                else return;
-
-                if (stop.WaitOne(ts, false))
-                {
-                    //Console.WriteLine("Stopped");
-                    return;
-                }
+                prevImage = frm.Image;
+                prevMessage = frm.Message;
             }
+
+            frm.FormClosed += new FormClosedEventHandler(frm_FormClosed);
+            doUpdate(prevImage, prevMessage);
+            ev1.Set();
+            frm.StartSplash();
         }
 
-		
-		/// <summary>
-		/// Show another Image
-		/// </summary>
-		/// <param name="image">the image to show</param>
-		public static void UpdateImage(System.Drawing.Image image)
-		{
-			nextevent.Image = image;
-			nextevent.Kind = (WSUpdate)((byte)nextevent.Kind | (byte)WSUpdate.Image);
-		}
-
-		/// <summary>
-		/// Show another Image
-		/// </summary>
-		/// <param name="image">the image to show</param>
-		public static void Update(System.Drawing.Image image, string message)
-		{
-			nextevent.Message = message;
-			nextevent.Image = image;
-			nextevent.Kind = WSUpdate.Both;
-		}
-
-		/// <summary>
-		/// Show a short Message about the current State
-		/// </summary>
-		/// <param name="msg">The Message you want to show</param>
-		public static void UpdateMessage(string msg)
-		{
-			nextevent.Message = msg;
-			nextevent.Kind = (WSUpdate)((byte)nextevent.Kind | (byte)WSUpdate.Message);
-		}
-
-		
-
-		/// <summary>
-		/// Returns the Size of the Dispalyed Image
-		/// </summary>
-		public static System.Drawing.Size ImageSize
-		{
-			get { return new System.Drawing.Size(64, 64); }
-		}
-	}
+        void frm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            lock (lockObj)
+            {
+                //if (t != null) t.Join();
+                t = null;
+                frm = null;
+                scr = null;
+            }
+        }
+    }
 }
